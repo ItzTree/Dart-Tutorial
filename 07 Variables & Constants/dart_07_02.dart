5520cc76a056ec2e3aafe902438920ca57feb5f0
ItzTree <97678209+ItzTree@users.noreply.github.com>
2022-10-19 16:01:33 +0900

    Create dart_07_02.dart

---
+++ 07 Variables & Constants/dart_07_02.dart
@@ -0,0 +1,9 @@
+void main() {
+  var myInt = 1;
+  var myDouble = 1.0;
+  var myString = "Dr.Sungwon";
+  // 저장되는 변수의 타입을 지정하지 않고 싶으면 var 이용
+  // var을 이용하여 변수를 선언하면 자동으로 타입 배정됨
+
+  print("$myInt $myDouble $myString");
+}
