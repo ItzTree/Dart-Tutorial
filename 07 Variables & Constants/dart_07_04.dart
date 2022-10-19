a13888e850aa1f052eb6a0855d99bb50213b6fae
ItzTree <97678209+ItzTree@users.noreply.github.com>
2022-10-19 16:46:55 +0900

    Create dart_07_04.dart

---
+++ 07 Variables & Constants/dart_07_04.dart
@@ -0,0 +1,9 @@
+void main() {
+  final String firstName = "Sungwon";
+  final secondName = "Lee";
+
+  const double myPi = 3.141592;
+  const changeRate = 1.3;
+
+  print("$firstName $secondName $myPi $changeRate");
+}
