a12b79a08dce69d1ae0df1a832e858b1e2315317
ItzTree <97678209+ItzTree@users.noreply.github.com>
2022-10-19 16:05:02 +0900

    Update dart_07_02.dart

--- 07 Variables & Constants/dart_07_02.dart
+++ 07 Variables & Constants/dart_07_02.dart
@@ -5,5 +5,12 @@ void main() {
   // 저장되는 변수의 타입을 지정하지 않고 싶으면 var 이용
   // var을 이용하여 변수를 선언하면 자동으로 타입 배정됨
 
+  myInt = 2.0;
+  // int형 변수에 실수 대입 불가
+  myDouble = 4;
+  // double형 변수에 정수 대입 가능
+  myString = 1;
+  // 문자열 변수에 실수 및 정수 대입 불가
+
   print("$myInt $myDouble $myString");
 }
