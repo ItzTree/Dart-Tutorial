4300d766ef296de1fdacb15027bbf9975f747099
ItzTree <97678209+ItzTree@users.noreply.github.com>
2022-10-21 18:07:22 +0900

    Create dart_10_05.dart

---
+++ 10 Boolean & Conditional Statement/dart_10_05.dart
@@ -0,0 +1,16 @@
+void main() {
+  var command = 'close';
+
+  switch (command) {
+    case 'close':
+    case 'CLOSE':
+      print("CLOSE");
+      break;
+    case 'open':
+    case 'OPEN':
+      print("OPEN");
+      break;
+    default:
+      print("default");
+  }
+}
