4d4ec96f949f27e9891f517ea811d8aa22bb431b
ItzTree <97678209+ItzTree@users.noreply.github.com>
2022-10-23 08:06:33 +0900

    Create dart_11_02.dart

---
+++ 11 Repetition (Loop)/dart_11_02.dart
@@ -0,0 +1,10 @@
+void main() {
+  var temp = 0;
+
+  for (var num1 = 1; num1 <= 9; num1++) {
+    for (var num2 = 1; num2 <= 9; num2++) {
+      temp = num1 * num2;
+      print("$num1 * $num2 = $temp");
+    }
+  }
+}
