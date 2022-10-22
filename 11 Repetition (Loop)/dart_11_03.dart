04d89d4cfe7dcad0a2a982f1e8adee8706fc16d6
ItzTree <97678209+ItzTree@users.noreply.github.com>
2022-10-23 08:32:07 +0900

    Create dart_11_03.dart

---
+++ 11 Repetition (Loop)/dart_11_03.dart
@@ -0,0 +1,7 @@
+void main() {
+  var oNum = 1;
+  for (var iNum = 1; iNum < 3; iNum++) {
+    print("[OUT] oNum is $oNum and iNum is $iNum");
+  }
+  print("[OUT] oNum is $oNum and iNum is $iNum");   // Error
+}
