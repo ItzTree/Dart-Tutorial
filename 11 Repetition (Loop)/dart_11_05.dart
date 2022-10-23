058f0f7a6d5e81bb8079c594ceb4071c511c07c5
ItzTree <97678209+ItzTree@users.noreply.github.com>
2022-10-23 10:35:23 +0900

    Create dart_11_05.dart

---
+++ 11 Repetition (Loop)/dart_11_05.dart
@@ -0,0 +1,11 @@
+void main() {
+  var sum = 0;
+  var num = 1;
+
+  do {
+    sum = sum + num;
+    print("sum is $sum and num is $num");
+    num++;
+  } while (num <= 10);
+  print("Finally, sum is $sum");
+}
