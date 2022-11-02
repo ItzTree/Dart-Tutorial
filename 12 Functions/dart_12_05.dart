f3a5e3bf9e0a4deadd57ac97159c762a57d1232a
ItzTree <97678209+ItzTree@users.noreply.github.com>
2022-11-02 11:35:52 +0900

    Create dart_12_05.dart

---
+++ 12 Functions/dart_12_05.dart
@@ -0,0 +1,10 @@
+int calcSum(int num1, int num2) {
+  var localSum = num1 + num2;
+  return localSum;
+}
+
+void main() {
+  var resNum = calcSum(1,1);
+  print("$resNum");
+  print("$localSum"); // Error
+}
