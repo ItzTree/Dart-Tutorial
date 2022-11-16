2b40fe21349f8ac7210cedf5b56c1289c535e811
ItzTree <97678209+ItzTree@users.noreply.github.com>
2022-11-16 14:48:44 +0900

    Create dart_14_05.dart

---
+++ 14 Class/dart_14_05.dart
@@ -0,0 +1,11 @@
+void main() {
+  bool bool1 = true;
+  var myString;
+
+  // Print boolean object
+  print("$bool1"); // true
+
+  // Convert boolean object to String
+  myString = bool1.toString();
+  print("$myString"); // true
+}
