0416a02f8dbf6365c2af646242a72038708056e4
ItzTree <97678209+ItzTree@users.noreply.github.com>
2022-11-16 14:37:26 +0900

    Create dart_14_03.dart

---
+++ 14 Class/dart_14_03.dart
@@ -0,0 +1,27 @@
+void main() {
+  var num;
+
+  // Returns the absolute value
+  num = 1.4.abs();
+  print("$num");
+
+  // Returns a string representation
+  num = 1.4.toString();
+  print("$num");
+
+  // Returns the greatest integer no greater than member data
+  num = 1.4.floor();
+  print("$num");
+
+  // Returns the greatest integer double value no greater than member data
+  num = 1.4.floorToDouble();
+  print("$num");
+
+  // Returns the greatest integer no greater than member data
+  num = 1.4.round();
+  print("$num");
+
+  // Returns the integer double value obtained by discarding any fractional digits
+  num = 1.4.truncateToDouble();
+  print("$num");
+}
