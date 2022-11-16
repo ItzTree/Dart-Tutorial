ab070997cc94d18472e5e663d6046559f60057a8
ItzTree <97678209+ItzTree@users.noreply.github.com>
2022-11-16 16:06:25 +0900

    Create dart_16_01.dart

---
+++ 16 Set/dart_16_01.dart
@@ -0,0 +1,31 @@
+void main() {
+  // #1 Simple List and Set
+  var myList1 = ["C++", "Dart", "Go", "Python", "Dart"];
+  print("#1.1 $myList1");
+  var mySet1  = {"C++", "Dart", "Go", "Python", "Dart"};
+  print("#1.2 $mySet1");
+
+  // #2 Empty Set and element addition
+  var mySet2 = new Set();
+  mySet2.add("C");
+  mySet2.addAll(mySet1);
+  print("#2 $mySet2 :: " + mySet2.length.toString());
+
+  // #3 ...? operator usage
+  var mySet3 = {...?mySet2, "R"};
+  print("#3 $mySet3");
+
+  // #4 contains() method
+  if(mySet3.contains("Dart") == true) {
+    print("#4 Dart language is included in mySet3");
+  }
+
+  // #5 union() method
+  var mySet4 = {"JavaScript", "TypeScript"};
+  var mySet5 = mySet3.union(mySet4);
+  print("#5 $mySet5");
+
+  // #6 dynamic Set for heterogeneous types
+  Set mySet6 = {"C++", "Dart", "Go", "Python", 1, 2, 3};
+  print("#6 $mySet6");
+}
