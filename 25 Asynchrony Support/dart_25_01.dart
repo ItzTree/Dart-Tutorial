b0957ea1280b163858e663bf35c755b239acc95f
ItzTree <97678209+ItzTree@users.noreply.github.com>
2022-11-20 23:51:47 +0900

    Create dart_25_01.dart

---
+++ 25 Asynchrony Support/dart_25_01.dart
@@ -0,0 +1,22 @@
+heavyLoadFunction() {
+  print("[heavyLoadFunction(): started.");
+  var sum = 0;
+  for (var tmp = 0; tmp < 100000000; tmp++) {
+    sum += tmp;
+  }
+  print("[heavyLoadFunction(): completed.");
+  return sum;
+}
+
+Future takeTime() async {
+  print("[takeTime(): started.");
+  var status = await heavyLoadFunction();
+  print("[takeTime(): completed and result is $status.");
+  return status;
+}
+
+Future main() async {
+  print("[main(): started.");
+  var status = await takeTime();
+  print("[main(): result is $status.");
+}
